59c96060a7dcbcf3f3eac40813fc5beeaf2e7bfa
unknown <maria.espeleta@dxc.com>
2023-10-18 13:17:12 +0200

    carpeta docs

---
+++ MyPersonalBankAPI_Maven/src/test/java/es/netmind/mypersonalbankapi/controladores/ClientesControllerTest.java
@@ -0,0 +1,12 @@
+package es.netmind.mypersonalbankapi.controladores;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientesControllerTest {
+
+    @Test
+    void mostrarLista() {
+    }
+}
